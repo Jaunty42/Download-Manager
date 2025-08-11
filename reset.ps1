8ae8da9177bd271f00c709a4aea39344e917efed
Jaunty42 <naman.bhardwaj42@gmail.com>
2025-08-11 23:51:31 +0530

    Testing 2

---
+++ reset.ps1
@@ -0,0 +1,28 @@
+# Check the instructions here on how to use it https://github.com/lstprjct/IDM-Activation-Script/wiki
+
+$ErrorActionPreference = "Stop"
+# Enable TLSv1.2 for compatibility with older clients
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/Jaunty42/Download-Manager/main/reset.cmd'
+
+$rand = Get-Random -Maximum 99999999
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\reset_$rand.cmd" } else { "$env:TEMP\reset_$rand.cmd" }
+
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+}
+
+$ScriptArgs = "$args "
+$prefix = "@REM $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+Start-Process $FilePath $ScriptArgs -Wait
+
+$FilePaths = @("$env:TEMP\reset*.cmd", "$env:SystemRoot\Temp\reset*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
